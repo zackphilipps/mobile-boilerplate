b63403e943d340442f56cbff8434730deb426c5e
Zack Philipps <zack@tribeswell.com>
2014-06-27 11:29:40 -0400

    removing outdatedBrowser

--- footer.php
+++ footer.php
@@ -2,12 +2,6 @@
   <p class="center">&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></p>
 </footer>
 
-<div id="outdated">
-   <h6>Your browser is out-of-date!</h6>
-   <p>Update your browser to view this website correctly. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Update my browser now </a></p>
-   <p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>
-</div>
-
 <?php wp_footer(); ?>
 
 <!--<script type="text/javascript">
@@ -26,11 +20,6 @@
 
 <script type="text/javascript">
   jQuery(document).ready(function($) {
-    outdatedBrowser({
-        bgColor: '#f25648',
-        color: '#ffffff',
-        lowerThan: 'transform'
-    });
     $('.nav-toggle').click(function() {
       $('nav ul.main-nav').slideToggle('fast');
       $(this).toggleClass('active');
