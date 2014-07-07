373f4658d7408a044065a094937150a0ac169a44
Zack Philipps <zack@tribeswell.com>
2014-07-07 16:39:03 -0400

    updating ionicons

---
+++ sidebar-{rename}.php
@@ -0,0 +1,5 @@
+<aside id="sidebar"
+       class="fourcol last"
+       role="navigation">
+  <p>This is the sidebar-{rename}.php file</p>
+</aside>
---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<aside id="sidebar"
+       class="fourcol last"
+       role="navigation">
+  <p>This is the sidebar.php file</p>
+</aside>
