0ea3796a6487f6d5573fd09c7d61addb9b1591cf
Zack Philipps <zack@tribeswell.com>
2014-07-28 09:48:21 -0400

    add .image-link, add toggles, move footer scripts to main, switch nav toggle to velocity, img height: auto; add wp-caption and post img styles, change silver to #ccc, add styles to google fonts

--- footer.php
+++ footer.php
@@ -23,50 +23,6 @@
 });*/
 </script>-->
 
-<script type="text/javascript">
-  jQuery(document).ready(function($) {
-    $('.nav-toggle').click(function() {
-      $('nav ul.main-nav').slideToggle('fast');
-      $(this).toggleClass('active');
-    });
-    $('.popup-link').magnificPopup({
-      type:'inline',
-      midClick: true
-    });
-    $('.popup-gallery').each(function() {
-        $(this).magnificPopup({
-            delegate: 'a',
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0,1]
-            },
-            image: {
-                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                titleSrc: function(item) {
-                    return item.el.attr('title');
-                }
-            }
-        });
-    });
-    $('.scroll-link').click(function(e) {
-      e.preventDefault();
-      $(this).blur();
-      var string = $(this).attr('href').split('#')[1];
-      $('#' + string)
-        .velocity('scroll', 400);
-    });
-    $('.slider').glide({
-      autoplay: false,
-      arrowRightText: '',
-      arrowLeftText: ''
-    });
-  });
-</script>
-
 <!-- Enable LiveReload. Change localhost to your IP address. Delete if not needed. -->
 <script src="//localhost:35729/livereload.js"></script>
 
