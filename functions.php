a47fc431647dcbaa112e16eae86b29749462f6ed
Zack Philipps <zackphilipps@Zacks-Mac-mini.local>
2014-10-31 09:45:16 -0400

    customize login screen further so that the logo links to the home page and the title is the site title

--- functions.php
+++ functions.php
@@ -223,6 +223,16 @@ function scratch_login_stylesheet() { ?>
 <?php }
 add_action( 'login_enqueue_scripts', 'scratch_login_stylesheet' );
 
+function scratch_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'scratch_login_logo_url' );
+
+function scratch_login_logo_url_title() {
+    return get_bloginfo('name');
+}
+add_filter( 'login_headertitle', 'scratch_login_logo_url_title' );
+
 
 
 
