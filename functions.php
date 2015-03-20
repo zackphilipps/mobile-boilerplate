5f7d5c217566520601d4f3e80787a29e73c0e9d6
Zack Philipps <zack@tribeswell.com>
2015-03-20 15:14:46 -0400

    Add `custom.css`.

--- functions.php
+++ functions.php
@@ -107,9 +107,9 @@ function mobile_meta() { ?>
   <!-- This script prevents links from opening in Mobile Safari. https://gist.github.com/1042026 -->
 
   <!-- The bottom part resumes the web app where the user left off -->
-          
+
   <!--<script>(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
-      
+
     if (window.navigator.standalone) {
       var setLastUrl = function() {
         localStorage['lastUrl'] = window.location;
@@ -134,21 +134,24 @@ function mobile_meta() { ?>
 
 <?php }
 
- add_action('wp_head', 'mobile_meta'); 
+ add_action('wp_head', 'mobile_meta');
 
 /* Theme CSS */
 
 function theme_styles() {
-    
+
     wp_register_style( 'scratch-main', get_template_directory_uri() . '/css/master.css', false, filemtime(dirname(__FILE__) . '/css/master.css') );
     wp_enqueue_style( 'scratch-main' );
-  
+
     global $wp_styles; /* call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way */
-  
+
     wp_register_style( 'scratch-ie', get_template_directory_uri() . '/css/ie.css', false, filemtime(dirname(__FILE__) . '/css/ie.css') );
     $wp_styles->add_data( 'scratch-ie', 'conditional', 'lt IE 9' );
     wp_enqueue_style( 'scratch-ie' );
-  
+
+    wp_register_style( 'scratch-custom', get_template_directory_uri() . 'custom.css', false, filemtime(dirname(__FILE__) . 'custom.css') );
+    wp_enqueue_style( 'scratch-custom' );
+
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
@@ -156,37 +159,37 @@ add_action( 'wp_enqueue_scripts', 'theme_styles' );
 /* Theme JavaScript */
 
 function theme_js() {
-    
+
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr-2.8.3.min.js', false, false, false );
 
     wp_register_script( 'scratch-main-concat', get_template_directory_uri() . '/js/concat/main.js', array('jquery'), filemtime(dirname(__FILE__) . '/js/concat/main.js'), true );
 
     wp_register_script( 'scratch-main-min', get_template_directory_uri() . '/js/compiled/main.min.js', array('jquery'), filemtime(dirname(__FILE__) . '/js/compiled/main.min.js'), true );
-  
+
     /* FOR DEVELOPMENT */
     wp_enqueue_script( 'scratch-main-concat' );
-  
+
     /* FOR PRODUCTION */
     /*wp_enqueue_script( 'scratch-main-min' );*/
-    
+
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_js' );
 
 /* Enable ACF Options Pages */
 
-if(function_exists('acf_add_options_page')) { 
- 
+if(function_exists('acf_add_options_page')) {
+
 	acf_add_options_page();
   acf_add_options_sub_page('Header');
 	acf_add_options_sub_page('Sidebar');
 	acf_add_options_sub_page('Footer');
- 
+
 }
 
 /* Enable Featured Image */
 
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support( 'post-thumbnails' );
 
 /* Enable Custom Menus */
 
